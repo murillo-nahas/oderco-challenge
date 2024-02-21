e8de8b16859054e10d72e4c6676b8d16f573f9a0
murillo.nahas <murillonahas@hotmail.com>
2024-02-20 22:04:45 -0300

    feat: img ats

--- components/Header/Header.vue
+++ components/Header/Header.vue
@@ -1,5 +1,9 @@
 <template>
-    <header class="py-6 px-12 border-b-2 border-[#DBA90D]">
-        <img src="~/assets/images/star-wars-logo.svg" />
-    </header>
+  <header class="py-6 px-12 border-b-2 border-borderYellow">
+    <img
+      src="~/assets/images/star-wars-logo.svg"
+      alt="Star Wars Logo"
+      loading="lazy"
+    />
+  </header>
 </template>
