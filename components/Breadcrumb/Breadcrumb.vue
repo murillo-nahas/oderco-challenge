97298cf6d266041f51ecf4b31196692c2fdff3fd
murillo.nahas <murillonahas@hotmail.com>
2024-02-21 22:26:46 -0300

    feat: breadcrumb component

---
+++ components/Breadcrumb/Breadcrumb.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="mt-12 w-4/5 m-auto">
+    <h1
+      class="text-textGrayLighter font-bold cursor-pointer hover:text-textGray"
+    >
+      {{ title }}
+    </h1>
+  </div>
+</template>
+
+<script setup lang="ts">
+const props = defineProps({
+  title: String,
+});
+</script>
