fbd7f6ab3259287b593f4ac3be0a919c24600570
murillo.nahas <murillonahas@hotmail.com>
2024-02-21 22:26:40 -0300

    feat: link to home in header

--- components/Header/Header.vue
+++ components/Header/Header.vue
@@ -1,9 +1,11 @@
 <template>
   <header class="py-6 px-12 border-b-2 border-borderYellow">
-    <img
-      src="~/assets/images/star-wars-logo.svg"
-      alt="Star Wars Logo"
-      loading="lazy"
-    />
+    <NuxtLink :to="`/home`">
+      <img
+        src="~/assets/images/star-wars-logo.svg"
+        alt="Star Wars Logo"
+        loading="lazy"
+      />
+    </NuxtLink>
   </header>
 </template>
