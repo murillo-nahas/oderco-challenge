b1eb90b454f690853d56396dd5faff8d63dd4e34
murillo.nahas <murillonahas@hotmail.com>
2024-03-08 15:29:48 -0300

    feat: get information button component

---
+++ components/GetInformationButton/GetInformationButton.vue
@@ -0,0 +1,13 @@
+<template>
+  <button
+    class="text-2xl px-12 py-3 rounded-3xl text-borderGrayLighter border-2 border-borderGrayLighter hover:text-black hover:bg-borderGrayLighter"
+  >
+    {{ title }}
+  </button>
+</template>
+"
+<script setup lang="ts">
+const props = defineProps({
+  title: String,
+});
+</script>
