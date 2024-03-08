1b949a46fdebc8ba66d838022edf573f1aae088d
murillo.nahas <murillonahas@hotmail.com>
2024-03-08 15:30:50 -0300

    feat(wip): informations by id page

--- pages/home/[id].vue
+++ pages/home/[id].vue
@@ -1,9 +1,34 @@
 <template>
-  <h1 class="text-white">Dados: {{ responseData }}</h1>
+  <Breadcrumb :title="'Home → Busca'" />
+  <main class="w-4/5 m-auto">
+    <div class="mt-12 flex flex-col justify-center items-center">
+      <p class="text-white text-3xl font-bold">Detalhes do personagem</p>
+      <p class="mt-10 text-borderYellow text-5xl font-bold">
+        {{ responseData.name }}
+      </p>
+    </div>
+
+    <div class="mt-16 w-full flex flex-wrap justify-between items-center">
+      <GetInformationButton
+        :title="'Detalhes'"
+        @click="getDetailsInformation"
+      />
+    </div>
+
+    <div class="mt-2">
+      <NuxtLink
+        class="text-borderYellow underline cursor-pointer hover:text-borderYellowHover"
+        :to="`/home`"
+        >Voltar aos outros personagens</NuxtLink
+      >
+    </div>
+  </main>
 </template>
 
 <script setup lang="ts">
-const { params }: any = useRoute();
+import type { RouteLocationNormalizedLoaded } from "vue-router";
+
+const { params }: RouteLocationNormalizedLoaded = useRoute();
 
 const config = useRuntimeConfig();
 
@@ -12,6 +37,16 @@ const { data: people }: any = await useFetch(
 );
 
 let responseData = people.value;
+
+async function getDetailsInformation() {
+  const detailsData: any = await $fetch(
+    `${config.public.API_PEOPLE_URL}/${params.id}`
+  );
+
+  responseData = detailsData;
+  console.log(responseData);
+}
+
 console.log(responseData);
 
 useHead({
