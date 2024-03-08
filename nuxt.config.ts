e7f717aaa35e9eb990a291b89cff7ea025ddb247
murillo.nahas <murillonahas@hotmail.com>
2024-03-07 21:16:58 -0300

    feat: working with .env like (runtimeConfig)

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,9 @@
 export default defineNuxtConfig({
+  runtimeConfig: {
+    public: {
+      API_PEOPLE_URL: process.env.API_PEOPLE_URL,
+    },
+  },
   devtools: { enabled: true },
   pages: true,
   components: true,
@@ -11,12 +16,15 @@ export default defineNuxtConfig({
   },
   app: {
     head: {
-      charset: 'utf-8',
-      viewport: 'width=device-width, initial-scale=1',
+      charset: "utf-8",
+      viewport: "width=device-width, initial-scale=1",
       meta: [
-        { name: 'description', content: 'Projeto Star Wars SWAPI - consulte as principais informações dos seus personagens.' }
+        {
+          name: "description",
+          content:
+            "Projeto Star Wars SWAPI - consulte as principais informações dos seus personagens.",
+        },
       ],
-    }
+    },
   },
-  
 });
